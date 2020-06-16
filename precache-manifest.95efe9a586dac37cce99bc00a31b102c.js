5d9bc331b8d28a889b344e5379b9d2b72ba00bbb
Suyash <dubli.suyash2202@gmail.com>
2020-06-16 18:00:27 +0530

    Updates

---
+++ precache-manifest.95efe9a586dac37cce99bc00a31b102c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc666c59212fb10f9b64de3db8b3f51",
+    "url": "/smart-brains/index.html"
+  },
+  {
+    "revision": "895be06681af70a7dcd2",
+    "url": "/smart-brains/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "52d079f9a762dff57ab6",
+    "url": "/smart-brains/static/css/main.ff0b245d.chunk.css"
+  },
+  {
+    "revision": "895be06681af70a7dcd2",
+    "url": "/smart-brains/static/js/2.caf5a01d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-brains/static/js/2.caf5a01d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52d079f9a762dff57ab6",
+    "url": "/smart-brains/static/js/main.59bed254.chunk.js"
+  },
+  {
+    "revision": "0eb5c2b94f4ac0a58dfd",
+    "url": "/smart-brains/static/js/runtime-main.3b68f55a.js"
+  }
+]);
